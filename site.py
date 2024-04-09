5ac85514deb34ee61e1d0da0b9c310821f45556e
TimofeySar <tsaratovkin@gmail.com>
2024-04-09 21:39:32 +0300

    маршруты

--- site.py
+++ site.py
@@ -1,15 +1,38 @@
 from flask import Flask, render_template
 from flask.helpers import url_for
+import datetime
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'proect_ot_timochi_i_iluchu'
+app.config['PERMANENT_SESSION_LIFETIME'] = datetime.timedelta(
+    days=365)
 
-@app.route("/")
+@app.route("/fifth")
 def home():
     return render_template("home.html")
 
-@app.route('/action')
-def another_page():
-    return render_template('index.html')
+@app.route("/forth")
+def home_1():
+    return render_template("home_1.html")
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route("/first")
+def home_2():
+    return render_template("trip1.html")
+
+@app.route("/second")
+def home_3():
+    return render_template("jor2.html")
+
+@app.route("/third")
+def home_4():
+    return render_template("trip3.html")
+
+@app.route("/six")
+def home_7():
+    return render_template("gome_6.html")
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
+
