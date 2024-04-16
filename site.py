0808eade18444b79fb8bad7089a4d2f44b1a5552
TimofeySar <tsaratovkin@gmail.com>
2024-04-16 20:11:19 +0300

    commit

--- site.py
+++ site.py
@@ -1,13 +1,6 @@
-from flask import Flask, redirect, render_template, request, make_response, session
-from data import db_session
-from flask_login import LoginManager, current_user, login_user, login_required, logout_user
-from data.users import User
-from data.jobs import Jobs
+from flask import Flask, render_template
+from flask.helpers import url_for
 import datetime
-from forms.LoginForm import LoginForm
-from forms.JobForm import JobForm
-from forms.RegistForm import RegistForm
-
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'proect_ot_timochi_i_iluchu'
@@ -48,12 +41,6 @@ def home_4():
 @app.route("/six")
 def home_7():
     return render_template("gome_6.html")
-
-@app.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return redirect("/")
 
 
 if __name__ == '__main__':
